928deb5cebdc0418befb080c236ec4d5c954d839
Võ Thành Giỏi <23130083@st.hcmuaf.edu.vn>
2026-05-28 22:28:24 +0700

    delete file

--- utils/Difficulty.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public enum Difficulty {
-    EASY(150), MEDIUM(100), HARD(60);
-    private final int delay;
-    Difficulty(int delay) { this.delay = delay; }
-    public int getDelay() { return delay; }
-}
